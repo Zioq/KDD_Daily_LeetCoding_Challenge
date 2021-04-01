1a0000b06bb4140adc5b4f6f49588862819f962f
yongjukwon <michaelkyj@gmail.com>
2021-03-31 23:47:10 -0700

    [Yongju] Wednesday submission 2

---
+++ 0329-0404 (Week_1)/Yongju/70.Climbing Stairs/l_70_climbing_stairs.java
@@ -0,0 +1,27 @@
+/**
+ * Leetcode, 70. Climbing Stairs
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+
+class Solution {
+  /**
+   * Dynamic programming
+   * Can be solved with recursion (brute force) & memoization
+   * but bottom-up approach performs the best
+   */
+  public int climbStairs(int n) {
+      if (n == 1) return 1;
+      if (n == 2) return 2;
+      
+      int[] ways = new int[n + 1];
+      
+      ways[1] = 1;
+      ways[2] = 2;
+      
+      for (int i = 3; i <= n; ++i)  {
+          ways[i] = ways[i - 1] + ways[i - 2];
+      }
+      
+      return ways[n];
+  }
+}
