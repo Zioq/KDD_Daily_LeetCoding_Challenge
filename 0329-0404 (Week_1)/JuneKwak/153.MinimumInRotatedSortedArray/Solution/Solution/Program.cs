5632e2f85f04d0ccee08ad600ebf6a586103189d
Hyung June Kwak <kwakdent95@gmail.com>
2021-04-04 20:43:13 -0700

    FindMinInRoatedSortedArray

---
+++ 0329-0404 (Week_1)/JuneKwak/153.MinimumInRotatedSortedArray/Solution/Solution/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] a = { 3, 4, 5, 1, 2 };
+            Console.WriteLine($"Minimum element of a is: {solution.FindMin(a)}");
+
+            int[] b = { 4, 5, 6, 7, 0, 1, 2 };
+            Console.WriteLine($"Minimum element of b is: {solution.FindMin(b)}");
+
+
+            int[] c = { 11, 13, 15, 17 };
+            Console.WriteLine($"Minimum element of c is: {solution.FindMin(c)}");
+        }
+
+        public class Solution
+        {
+            public int FindMin(int[] nums)
+            {
+                Array.Sort(nums);
+                return nums[0];
+            }
+        }
+    }
+}
