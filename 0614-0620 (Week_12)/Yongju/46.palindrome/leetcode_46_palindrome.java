e9b78a401e65de93c27d2743ced74909028df17f
yongjukwon <michaelkyj@gmail.com>
2021-06-20 07:36:39 -0700

    [Yongju] Add 210620 leetcode_46_palindrome

---
+++ 0614-0620 (Week_12)/Yongju/46.palindrome/leetcode_46_palindrome.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+  public List<List<Integer>> permute(int[] nums) {
+      List<Integer> inner = new ArrayList<>();
+      List<List<Integer>> result = new ArrayList<>();
+      boolean[] used = new boolean[nums.length];
+      
+      helper(nums, used, inner, result);
+      
+      return result;
+  }
+  
+  public void helper(int[] nums, boolean[] used, List<Integer> inner, List<List<Integer>> result) {
+      
+      if (inner.size() == nums.length) {
+          result.add(new ArrayList<>(inner)); // Since it is a reference
+          return;
+      }
+      
+      for (int i = 0; i < nums.length; ++i) {
+          if (!used[i]) {
+              // track used indexes
+              used[i] = true;
+              inner.add(nums[i]);
+
+              helper(nums, used, inner, result);
+
+              used[i] = false;
+              inner.remove(inner.size() - 1);
+          }
+      }
+  }
+}
